caa13c98c90901ef45c3399d8b5a68372cf950a0
Colin Daugherty <colinddaugherty@gmail.com>
2019-10-25 21:28:29 -0400

    testing with regex

--- tests/receiving_message.py
+++ tests/receiving_message.py
@@ -2,8 +2,11 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib
 import json
 import requests
+import re
 
 url = "https://api.groupme.com/v3/bots/post"
+
+meme_regex = re.compile("/(?!meme)(!meme)/g")
 
 class ExampleJSONHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -14,13 +17,20 @@ class ExampleJSONHandler(BaseHTTPRequestHandler):
         self.end_headers()
         if data['name'] != "test beep boop":
             self.wfile.write("In the JSON you sent me, data['foo'][2] is {}\n".format(data['text'][2]).encode('utf-8'))
-            message = "This is the message I received: {}\n".format(data['text'])
-
-            data = {"bot_id": "31283ec6a67b96f542641a4aa9", "text": str(message)}
-            r = requests.post(url, json=data)
-            self.wfile.write("\nThis is the post request I just tried-\n")
-            self.wfile.write(r.text)
-            self.wfile.write("\n")
+            if meme_regex.match(data['text']) != None:
+                message = "You just asked me for a meme but I can't do that yet :'("
+                data = {"bot_id": "31283ec6a67b96f542641a4aa9", "text": str(message)}
+                r = requests.post(url, json=data)
+                self.wfile.write("\nThis is the post request I just tried-\n")
+                self.wfile.write(r.text)
+                self.wfile.write("\n")
+            else:
+                message = "This is the message I received: {}\n".format(data['text'])
+                data = {"bot_id": "31283ec6a67b96f542641a4aa9", "text": str(message)}
+                r = requests.post(url, json=data)
+                self.wfile.write("\nThis is the post request I just tried-\n")
+                self.wfile.write(r.text)
+                self.wfile.write("\n")
 
 server = HTTPServer(("192.168.0.69", 2000), ExampleJSONHandler)
 server.serve_forever()
