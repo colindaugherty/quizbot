b0aef269e3edf1c4131dd07a5c705f96fa2fabe9
Colin Daugherty <colinddaugherty@gmail.com>
2019-10-25 21:14:08 -0400

    ok the break solution didnt work trying again

--- tests/receiving_message.py
+++ tests/receiving_message.py
@@ -12,16 +12,15 @@ class ExampleJSONHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header(b'Content-type', b'text/plain')
         self.end_headers()
-        if data['name'] == "test beep boop":
-            break
-        self.wfile.write("In the JSON you sent me, data['foo'][2] is {}\n".format(data['text'][2]).encode('utf-8'))
-        message = "This is the message I received {}\n".format(data['text'])
+        if data['name'] != "test beep boop":
+            self.wfile.write("In the JSON you sent me, data['foo'][2] is {}\n".format(data['text'][2]).encode('utf-8'))
+            message = "This is the message I received {}\n".format(data['text'])
 
-        data = {"bot_id": "31283ec6a67b96f542641a4aa9", "text": str(message)}
-        r = requests.post(url, json=data)
-        self.wfile.write("\nThis is the post request I just tried-\n")
-        self.wfile.write(r.text)
-        self.wfile.write("\n")
+            data = {"bot_id": "31283ec6a67b96f542641a4aa9", "text": str(message)}
+            r = requests.post(url, json=data)
+            self.wfile.write("\nThis is the post request I just tried-\n")
+            self.wfile.write(r.text)
+            self.wfile.write("\n")
 
 server = HTTPServer(("192.168.0.69", 2000), ExampleJSONHandler)
 server.serve_forever()
