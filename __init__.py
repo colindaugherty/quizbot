7c95824ee26b462701260b576537751f073c5cd3
Colin Daugherty <colinddaugherty@gmail.com>
2019-10-26 00:10:50 -0400

    still trying to figure out the import error

---
+++ __init__.py
@@ -0,0 +1,1 @@
+#the second idk man of the night
--- empuorg/message_routing.py
+++ empuorg/message_routing.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 import json
-import empuorg.Empuorg as empuorg
+import groupmebot.empuorg.empuorg as empuorg
 
 class MessageRouter(BaseHTTPRequestHandler):
 
