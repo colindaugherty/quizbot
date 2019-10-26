1d40c91c6e49f7e9a5b816f7cc738d1c543c94b0
Colin Daugherty <colinddaugherty@gmail.com>
2019-10-26 03:42:45 -0400

    handling stickied posts

--- empuorg/empuorg.py
+++ empuorg/empuorg.py
@@ -70,7 +70,10 @@ class Empuorg():
         submission_list = []
         rand = random.randint(0,2)
         for submission in reddit.subreddit(subreddit).hot(limit=3):
-            submission_list.append(submission)
+            if submission.stickied != True:
+                submission_list.append(submission)
+            else:
+                print("Submission was a stickied post gross")
         print("Printing url link-\n")
         if submission_list[rand].selftext == "":
             print(submission_list[rand].url)
