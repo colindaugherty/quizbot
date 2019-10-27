01deaebaafb3943303fc94e6dc126ca34d55388e
Colin Daugherty <colinddaugherty@gmail.com>
2019-10-26 23:21:12 -0400

    i may have broken things

--- empuorg/message_routing.py
+++ empuorg/message_routing.py
@@ -9,6 +9,5 @@ class MessageRouter(BaseHTTPRequestHandler):
         content_len = int(self.headers['content-length'])
         post_body = self.rfile.read(content_len)
         data = json.loads(post_body)
-        
-        #when a request is recieved, process the response as JSON and pass relevant data to exbot class
+    
         empuorg.bot.receive_message(data['text'],data['attachments'],data['sender_id'])
