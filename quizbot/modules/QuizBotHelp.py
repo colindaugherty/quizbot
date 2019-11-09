69284bd5dd26a8d1cb2fca9fca4a2f5780da6846
Colin Daugherty <colinddaugherty@gmail.com>
2019-11-08 20:19:37 -0500

    this modularization thing is great

--- quizbot/modules/QuizBotHelp.py
+++ quizbot/modules/QuizBotHelp.py
@@ -2,7 +2,7 @@
 
 class QuizBotHelp:
     def __init__(self):
-        message = "QuizBot Bot Commands-\nVersion 0.2b\n"
+        message = "QuizBot Bot Commands-\nVersion 0.3b\n"
         message += "!meme - searches for a random meme from your meme suppliers in the config\n"
         message += "!info - prints information about the group\n"
         message += "!config - edits group config\n"
