c3b1c0b3463fc0fa3730b080ee1c16b95fabb002
Colin Daugherty <colinddaugherty@gmail.com>
2019-11-10 22:58:00 -0500

    finished opt in and out command

--- quizbot/modules/QuizBotHelp.py
+++ quizbot/modules/QuizBotHelp.py
@@ -2,9 +2,9 @@
 
 class QuizBotHelp:
     def __init__(self):
-        message = "QuizBot Bot Commands-\nVersion 0.3b\n"
+        message = "QuizBot Bot Commands-\nVersion 0.4b\n"
         message += "!meme - searches for a random meme from your meme suppliers in the config\n"
-        message += "!info - prints information about the group\n"
+        message += "!opt <in|out>- opt in and out of events run by the bot, include the event for more specific opting\n"
         message += "!config - edits group config\n"
         message += "!fred - get a message from Fred\n"
         message += "!help - displays help commands\n"
