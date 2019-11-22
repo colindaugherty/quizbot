95fbe2a4f0b433d8c092f1b41fdf96604bfaf832
Colin Daugherty <colindaugherty@Colins-Macbook-Pro.local>
2019-11-22 11:15:27 -0500

    this should let discord and groupme run together

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from threading import Thread
 
 # import quizbot.QuizBotGroupMe as QuizBotGroupMe
 import quizbot.QuizBotDiscord as QuizBot
+import quizbot.QuizBotGroupMe as QuizBotGroupMe
 
 if len(sys.argv) is 2: #config file is specified
     config_file = os.path.normpath(sys.argv[1])
@@ -14,10 +15,10 @@ else:
 with open(config_file) as data_file:
     config = json.load(data_file)
 
-# QuizBotGroupMe.init()
+QuizBotGroupMe.init()
 
-# QuizBotGroupMe.listen(port=config['listening_port'])
+QuizBotGroupMe.listen(port=config['listening_port'])
 
 quizbot = QuizBot.QuizBotDiscord()
 
-QuizBot.client.run(quizbot.init("NjQ0MTk2ODU3MTEwODU1Njkw.Xcwhzg.gDcKMzfGgnkUVck-UEjCWnqUr3E"))
+QuizBot.client.run(quizbot.init(config['discord_key']))
