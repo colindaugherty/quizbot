31ee80db5ce0292db4142d0db2f34551d7538980
Colin Daugherty <colindaugherty@Colins-Macbook-Pro.local>
2019-11-25 01:59:14 -0500

    my logs aren't working? weird

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import sys
 import os
-import json
+import json, logging
 from threading import Thread
 
 import quizbot.QuizBotDiscord as QuizBot
 import quizbot.QuizBotGroupMe as QuizBotGroupMe
+
+logging.basicConfig(level=logging.DEBUG,filename='logs/basic.log', filemode='w', format='QuizBot: %(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+logging.info("Hello World! This is the first real line of quizbot")
 
 if len(sys.argv) is 2: #config file is specified
     config_file = os.path.normpath(sys.argv[1])
